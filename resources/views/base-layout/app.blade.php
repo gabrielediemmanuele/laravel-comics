44ef24875dd76fa1f18d0ade3c1cbf14fea9b529
gabrielediemmanuele <gabriele.diemmanuele@outlook.it>
2023-10-12 16:37:26 +0200

    feat: add  navbar partials, home extends + BS

--- resources/views/base-layout/app.blade.php
+++ resources/views/base-layout/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>Laravel Comics</title>
 
   <!-- Fonts -->
   <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -15,7 +15,13 @@
 </head>
 
 <body>
-  <h1 class="text-danger">ciao!</h1>
+  <header>
+    @include('partials._navbar')
+  </header>
+  <main>
+    @yield('main-content')
+  </main>
+  <footer></footer>
 </body>
 
 </html>
